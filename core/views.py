885e3df3e85326c1002c28cd1ce5dce4f8ff817b
Alie <a.hodavand@yahoo.com>
2019-08-16 23:16:50 +0430

    create movie list view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Movie
+
+
+class MovieListView(ListView):
+    model = Movie
+    context_object_name = 'movies'
+    pagination = 10
