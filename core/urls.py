e844ae18fcf615f5e2a185be4fde319fd7aafc43
Alie <a.hodavand@yahoo.com>
2019-08-17 01:36:03 +0430

    add comments and detail url

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import MovieListView
+from .views import MovieListView, MovieDetailView
 
 
 app_name = 'core'
 urlpatterns = [
-    path('', MovieListView.as_view(), name='movie_list')
+    # list view
+    path('', MovieListView.as_view(), name='movie_list'),
+    # detail view
+    path('<slug:slug>-<int:movie_id>/',
+         MovieDetailView.as_view(), name='movie_detail'),
 ]
