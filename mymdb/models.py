d74b41a6c630951f4bcc38e0dcb5556e84f1dffd
Alie <a.hodavand@yahoo.com>
2019-08-17 13:48:16 +0430

    add Person model

--- mymdb/models.py
+++ mymdb/models.py
@@ -25,3 +25,17 @@ class AbstractModel(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Person(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    born = models.DateField()
+    died = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('first_name', 'last_name')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
