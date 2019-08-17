05b2d1634126417301c6c8463839e8cf6f531655
Alie <a.hodavand@yahoo.com>
2019-08-18 00:29:38 +0430

    add PersonAdmin class

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Movie
+from .models import Movie, Person
 
 
 class MovieAdmin(admin.ModelAdmin):
-    fields = ('title', 'slug', 'description',
+    fields = ('title', 'slug', 'description', 'director',
               'year', 'image', 'runtime', 'rating', 'website')
     list_display = ('title', 'rating', 'runtime', 'year')
     list_filter = ('rating', 'year')
@@ -12,4 +12,11 @@ class MovieAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'born', 'died')
+    list_filter = ('first_name', 'last_name', 'born')
+    search_fields = ['first_name', 'last_name']
+
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Movie, MovieAdmin)
