0d2886b17e5f7190d0e7a3195d07dccf36c4ba1a
Alie <a.hodavand@yahoo.com>
2019-08-18 00:33:32 +0430

    add person_detail url

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import MovieListView, MovieDetailView
+from .views import MovieListView, MovieDetailView, PersonDetailView
 
 
 app_name = 'core'
@@ -10,4 +10,6 @@ urlpatterns = [
     # detail view
     path('<slug:slug>-<int:movie_id>/',
          MovieDetailView.as_view(), name='movie_detail'),
+    # person detail
+    path('person-<int:id>/', PersonDetailView.as_view(), name='person_detail'),
 ]
