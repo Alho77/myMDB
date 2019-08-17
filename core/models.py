6547e1ee9353fe6545f2a52324a05b2572aa627d
Alie <a.hodavand@yahoo.com>
2019-08-17 15:12:27 +0430

    add director, actors, and Role class

--- core/models.py
+++ core/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
-from mymdb.models import AbstractModel
+from mymdb.models import AbstractModel, Person
 
 
 class Movie(AbstractModel):
     runtime = models.PositiveIntegerField()
+    director = models.ForeignKey(
+        to=Person, on_delete=models.SET_NULL, related_name='directed', null=True, blank=True)
+    actors = models.ManyToManyField(
+        to=Person, related_name='acting_credits', through='Role', blank=True)
+
+
+class Role(models.Model):
+    person = models.ForeignKey(to=Person, on_delete=models.DO_NOTHING)
+    movie = models.ForeignKey(to=Movie, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together = ('movie', 'person', 'name')
+
+    def __str__(self):
+        return f'{self.movie_id} - {self.person_id}: {self.name}'
