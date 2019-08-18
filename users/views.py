5e5d608614e6d6d8a1f2625052b4d385c9840290
Alie <a.hodavand@yahoo.com>
2019-08-18 19:26:56 +0430

    create logout_view method

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.forms import ValidationError
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.template.loader import render_to_string
+from django.contrib.auth.decorators import login_required
 
 
 def register_handler(request, form, header):
@@ -39,3 +40,11 @@ def login_view(request):
         data['html_form'] = render_to_string(
             'users/register_login.html', context, request)
         return JsonResponse(data)
+
+
+@login_required
+def logout_view(request):
+    """Logout handler"""
+
+    logout(request)
+    return redirect(reverse('core:movie_list'))
