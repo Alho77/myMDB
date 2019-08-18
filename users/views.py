08a3d99d31d8e4dab342ab19ce5806545b985db4
Alie <a.hodavand@yahoo.com>
2019-08-18 10:17:59 +0430

    create register_handler method

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+from django.template.loader import render_to_string
 
-# Create your views here.
+
+def register_handler(request, form, header):
+    """For handling user egistering in modal dialog"""
+
+    data = dict()
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            data['is_valid'] = True
+        else:
+            data['is_valid'] = False
+
+    context = {'form': form, 'header': header}
+    data['html_form'] = render_to_string(
+        'users/register_login.html', context, request)
+    return JsonResponse(data)
