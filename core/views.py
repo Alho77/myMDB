5d2d5b36c54706b7602c804606db7e474b67bdd9
Alie <a.hodavand@yahoo.com>
2019-08-20 00:56:10 +0430

    overwrite MovieDetail's get_context_data method

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import DetailView, ListView
+from django.shortcuts import reverse
 
-from .models import Movie, Person
+from .models import Movie, Person, Vote
+from .forms import VoteForm
 
 
 class MovieListView(ListView):
@@ -15,6 +17,22 @@ class MovieDetailView(DetailView):
     query_pk_and_slug = True
     pk_url_kwarg = "movie_id"
     slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        if user.is_authenticated():
+            vote = Vote.objects.get_vote_or_unsaved_blank_vote(
+                self.object, user)
+            if vote.id:
+                context['vote_form_url'] = reverse('core:vote_update', kwargs={
+                                                   'movie_id': vote.movie.id, 'id': vote.id})
+            else:
+                context['vote_form_url'] = reverse('core:vote_create', kwargs={
+                                                   'movie_id': self.object.id})
+            context['vote_form'] = VoteForm(instance=vote)
+
+            return context
 
 
 class PersonDetailView(DetailView):
