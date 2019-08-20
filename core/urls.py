83a22d1d2c0e313ea74c67d2026900c9d562685d
Alie <a.hodavand@yahoo.com>
2019-08-20 12:18:07 +0430

    add create vote and update vote urls

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
 
-from .views import MovieListView, MovieDetailView, PersonDetailView
+from . import views
 
 
 app_name = 'core'
 urlpatterns = [
     # list view
-    path('', MovieListView.as_view(), name='movie_list'),
+    path('', views.MovieListView.as_view(), name='movie_list'),
     # detail view
     path('<slug:slug>-<int:movie_id>/',
-         MovieDetailView.as_view(), name='movie_detail'),
+         views.MovieDetailView.as_view(), name='movie_detail'),
     # person detail
-    path('person-<int:id>/', PersonDetailView.as_view(), name='person_detail'),
+    path('person-<int:id>/', views.PersonDetailView.as_view(), name='person_detail'),
+    # create vote
+    path('movie_<slug:slug>/vote/',
+         views.VoteCreateView.as_view(), name='vote_create'),
+    # update vote
+    path('movie_<slug:slug>/vote_<int:id>/',
+         views.VoteUpdateView.as_view(), name='vote_update'),
 ]
