3c12c67c237ffe2237347bde44f2066e5b27c7b2
Alie <a.hodavand@yahoo.com>
2019-08-21 14:39:16 +0430

    clean file

--- core/forms.py
+++ core/forms.py
@@ -1,19 +1,1 @@
 from django import forms
-from django.contrib.auth import get_user_model
-
-from .models import Movie, Vote
-
-User = get_user_model()
-
-
-class VoteForm(forms.ModelForm):
-    user = forms.ModelChoiceField(
-        User.objects.all(), widget=forms.HiddenInput, disabled=True)
-    movie = forms.ModelChoiceField(
-        Movie.objects.all(), widget=forms.HiddenInput, disabled=True)
-    value = forms.ChoiceField(
-        choices=Vote.CHOICE, widget=forms.RadioSelect, label='Vote')
-
-    class Meta:
-        model = Vote
-        fields = ('user', 'movie', 'value')
