e727732117a1237d3c51fd91c1c2291a583c3022
Alie <a.hodavand@yahoo.com>
2019-08-21 14:40:58 +0430

    remove vote urls

--- core/urls.py
+++ core/urls.py
@@ -12,10 +12,4 @@ urlpatterns = [
          views.MovieDetailView.as_view(), name='movie_detail'),
     # person detail
     path('person-<int:id>/', views.PersonDetailView.as_view(), name='person_detail'),
-    # create vote
-    path('movie_<slug:slug>/vote/',
-         views.VoteCreateView.as_view(), name='vote_create'),
-    # update vote
-    path('movie_<slug:slug>/vote_<int:id>/',
-         views.VoteUpdateView.as_view(), name='vote_update'),
 ]
