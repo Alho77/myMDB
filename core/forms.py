fe9c8a767c65d0ea2d5496b6d4286b18cc3e69d5
Alie <a.hodavand@yahoo.com>
2019-08-23 13:30:57 +0430

    create MoiveImageForm class

--- core/forms.py
+++ core/forms.py
@@ -1,1 +1,10 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+from .models import MovieImage, Movie
+
+
+class MovieImageForm(forms.ModelForm):
+    class Meta:
+        model = MovieImage
+        fields = ('image', )
