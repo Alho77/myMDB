91757d39f2cbdee105e036d60271ec99581afe41
Alie <a.hodavand@yahoo.com>
2019-08-25 00:50:58 +0430

    create CategoryInline and MovieImageAdmin classes. Add them to proper classes

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
-from .models import Movie, Person, MovieImage
+from . import models
+
+
+class CategoryInline(admin.TabularInline):
+    model = models.CategoryRelation
+    extra = 1
+
+
+class RoleInline(admin.TabularInline):
+    model = models.Role
+    extra = 1
 
 
 class MovieAdmin(admin.ModelAdmin):
@@ -8,16 +18,32 @@ class MovieAdmin(admin.ModelAdmin):
               'year', 'image', 'runtime', 'rating', 'website')
     list_display = ('title', 'rating', 'runtime', 'year')
     list_filter = ('rating', 'year')
+    inlines = (RoleInline, CategoryInline)
     search_fields = ['title', ]
     prepopulated_fields = {'slug': ('title',)}
+
+
+class MovieImageAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'user', 'uploaded_at')
+    list_filter = ('movie', 'user', 'uploaded_at')
+    search_fields = ('movie', 'user')
 
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'born', 'died')
     list_filter = ('first_name', 'last_name', 'born')
+    inlines = (RoleInline,)
     search_fields = ['first_name', 'last_name']
 
 
-admin.site.register(Person, PersonAdmin)
-admin.site.register(Movie, MovieAdmin)
-admin.site.register(MovieImage)
+class CategoryAdmin(admin.ModelAdmin):
+    list_filter = ('name',)
+    inlines = (CategoryInline,)
+    search_fields = ('name',)
+
+
+admin.site.register(models.Person, PersonAdmin)
+admin.site.register(models.Movie, MovieAdmin)
+admin.site.register(models.MovieImage, MovieImageAdmin)
+admin.site.register(models.Role)
+admin.site.register(models.Category, CategoryAdmin)
